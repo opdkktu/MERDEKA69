2c243a7535ea47b190c072079837e9c37f64780b
opdkktu <opd.kktu@moh.gov.my>
2026-08-16 08:35:24 +0800

    Update API_URL to deployed Google Apps Script URL

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 const APP_CONFIG = {
   // Paste your deployed Google Apps Script Web App URL here.
   // Get this after Deploy > New deployment > Web App (see README, Step 4).
-  API_URL: "YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL",
+  API_URL: "https://script.google.com/macros/s/AKfycbyaDjByj8zd75vcthe6zACgljAck8u39BnTFymnEq_VlT7TZI0vo_GWtl8bVMM83VR9/exec",
 
   APP_NAME: "MERDEKA MINI-GAMES",
 
